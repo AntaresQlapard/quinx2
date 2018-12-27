07ac10cb60b00f4a5228c4485b4d61600d03df36
AntaresQlapard <46163003+AntaresQlapard@users.noreply.github.com>
2018-12-27 20:08:59 +0600

    Add files via upload

---
+++ quinx.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asynco
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "?")
+@client.event
+async def on_ready():
+    print("Thankyou For Using Video Bot")
+	await client.change_presence(game=discord.Game(name="prefix ?")
+	
+@client.event
+async def on_message(message):
+    if message.content.startswith('?hello'):
+	    msg = 'Hello' (0.author.mention) Hows your day?'.format(message)
+		await client.send_message(message.channel ,msg)
+		
+client.run(os.getenv('rafsan'))
